4ebfe59d83b3e931040753a62847297c434375f0
Code5linger <sakib.ahmed.1708@gmail.com>
2024-09-08 01:30:36 +0600

    Product bussines type

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -4,21 +4,20 @@ import getAllProductsQuery from '../utils/queries/get-all-products';
 import { ProductConnection } from '../schema';
 
 import { normalizeProduct } from '../utils/normalize';
+import { Product } from '../../common/types/product';
 
 type ReturnType = {
   products: ProductConnection;
 };
 
-const getAllProducts = async (): Promise<any> => {
+const getAllProducts = async (): Promise<Product[]> => {
   const { data } = await fetchApi<ReturnType>({
     query: getAllProductsQuery,
   });
 
   const products =
-    data.products.edges.map(({ node: product }) => {
-      return normalizeProduct(product);
-    }) ?? [];
-
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+    [];
   return products;
 };
 
