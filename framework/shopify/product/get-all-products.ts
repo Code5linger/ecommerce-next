0d55b3ea5f6cbc7698e56001f08d1e221123c582
Code5linger <sakib.ahmed.1708@gmail.com>
2024-09-08 00:06:52 +0600

    Map product from connection type

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,11 +1,22 @@
 import fetchApi from '../utils/fetch-api';
-import getAllProductsQuery from '../utils/queries/get-all-products';
 
-const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi({
+import getAllProductsQuery from '../utils/queries/get-all-products';
+import { ProductConnection } from '../schema';
+
+type ReturnType = {
+  products: ProductConnection;
+};
+
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<ReturnType>({
     query: getAllProductsQuery,
   });
-  return products.data;
+
+  const products = data.products.edges.map(({ node: product }) => {
+    return product;
+  });
+
+  return products;
 };
 
 export default getAllProducts;
--- framework/shopify/utils/fetch-api.ts
+++ framework/shopify/utils/fetch-api.ts
@@ -1,4 +1,10 @@
-const fetchApi = async ({ query }: { query: string }) => {
+type FetcherResult<T> = { data: T };
+
+const fetchApi = async <T>({
+  query,
+}: {
+  query: string;
+}): Promise<FetcherResult<T>> => {
   const url = 'http://localhost:4000/graphql';
 
   const res = await fetch(url, {
@@ -12,7 +18,7 @@ const fetchApi = async ({ query }: { query: string }) => {
   });
 
   const { data, errors } = await res.json();
-  
+
   if (errors) {
     throw new Error(errors[0].message ?? errors.message);
   }
