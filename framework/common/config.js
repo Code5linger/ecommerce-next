80d8c0f58d3e1bd3b838208413ede58270fcc77f
Code5linger <sakib.ahmed.1708@gmail.com>
2024-09-08 04:04:56 +0600

    !

---
+++ framework/common/config.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable */
+
+const path = require('path');
+const merge = require('deepmerge');
+
+function withFrameworkConfig(defaultConfig = {}) {
+  const framework = 'bigcommerce';
+
+  const frameworkNextConfig = require(path.join(
+    '../',
+    framework,
+    'next.config'
+  ));
+  const config = merge(defaultConfig, frameworkNextConfig);
+
+  return config;
+}
+
+module.exports = { withFrameworkConfig };
--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+/* eslint-disable */
+
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
   i18n: {
     locales: ['en-US', 'es'],
     defaultLocale: 'en-US',
   },
-};
+});
 
 console.log('next.config.js', JSON.stringify(module.exports, null, 2));
